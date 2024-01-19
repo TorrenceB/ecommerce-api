2dce875a663224582b3b9780a560f457cc933b99
Torrence Brown <torrenceb90@gmail.com>
2024-01-18 19:17:39 -0700

    update port number

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 443;
+// const port = process.env.PORT || 443;
+const port = 80;
 const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname)));
