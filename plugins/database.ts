1ed5bc194e8fadb6516f795d3d0f6660a39e0633
TorrenceB <torrenceb90@gmail.com>
2025-02-09 00:11:47 +0000

    bug fix, api now running in container

--- plugins/database.ts
+++ plugins/database.ts
@@ -1,2 +1,15 @@
+// import { Client } from 'pg'
 
+// const client = new Client({
+//     user: process.env.POSTGRES_USER,
+//     host: process.env.POSTGRES_HOST,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
+//     // port: process.env.POSTGRES_PORT
+// })
 
+// client.connect()
+
+const client = {}
+
+export default client
--- routes/user.ts
+++ routes/user.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import db from '../plugins/database'
 
 const router = express.Router();
 
 router.get('/user/:id', (req, res, next) => {
     console.log('[user id:]', req.params.id)
+    console.log('[db]:', db)
 })
 
 export default router;
