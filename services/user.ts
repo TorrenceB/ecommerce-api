2d2a96394769df321c8b10c991882f9013e13aef
TorrenceB <torrenceb90@gmail.com>
2025-02-13 02:03:27 +0000

    remove logs

--- services/user.ts
+++ services/user.ts
@@ -5,8 +5,6 @@ const getUser = async (id: string) => {
         const response = await client.query(`SELECT * FROM "user" WHERE id = ${id}`)
         const user = response.rows[0]
 
-        console.log(`User retreived successfully`)
-
         return user
     } catch (error) {
         console.error(`[mh-api]: @getUser: ${error.message}`)
